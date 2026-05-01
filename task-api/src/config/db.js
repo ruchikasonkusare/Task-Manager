0bcafbf26ce617840e56b9bb9f1442411f2f0cf7
ruchikasonkusare <sonkusareruchika8@gmail.com>
2026-05-01 13:27:37 +0530

    Added db.js

---
+++ task-api/src/config/db.js
@@ -0,0 +1,24 @@
+const {Pool} = require('pg');
+
+require('dotenv').config();
+
+const pool= new Pool({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT
+})
+
+pool.connect((err,client,release)=>{
+    if(err){
+        console.error("Database connection error",err.message);
+    }
+    else{
+        console.log("Database connected successfully.");
+        release();
+    }
+})
+
+
+module.exports=pool;
